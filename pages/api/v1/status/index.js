3d92c0df4e5a99310a6054b73382e19945017b78
narciso.gomes <narcisoalmeida25@gmail.com>
2024-05-08 00:23:57 -0300

    ADD: env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,7 @@
 import database from "../../../../infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log("RESULTADO: ", result);
+  const result = await database.query("SELECT 1 + 1 as SUM;");
   response
     .status(200)
     .json({ chave: "Alunos do curso.dev são pessoas acima da média." });
