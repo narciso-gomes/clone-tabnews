3a77f89d2b3c52eda0dc761a81099348764582dc
narciso.gomes <narcisoalmeida25@gmail.com>
2024-05-07 23:42:17 -0300

    ADD: bd connection

--- models/calculadora.js
+++
@@ -1,8 +0,0 @@
-function somar(arg1, arg2){
-  if(typeof arg1 !== 'number' || typeof arg2 !== 'number'){
-    return 'Erro'
-  }
-  return arg1 + arg2;
-}
-
-exports.somar = somar;
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-function status(request, response){
-  response.status(200).json({chave: "Alunos do curso.dev são pessoas acima da média."})
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log("RESULTADO: ", result);
+  response
+    .status(200)
+    .json({ chave: "Alunos do curso.dev são pessoas acima da média." });
 }
 
-export default status
+export default status;
