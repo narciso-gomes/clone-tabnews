56a1acc5ab69d9096a5ec5d08735b4d0af252cbc
narcisogomes <narcisoalmeida25@gmail.com>
2024-05-11 11:59:31 -0300

    ADD: return updatedAt api status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,10 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as SUM;");
-  response
-    .status(200)
-    .json({ chave: "Alunos do curso.dev são pessoas acima da média." });
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+  });
 }
 
 export default status;
