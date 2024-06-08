e456174559059b5f3f3972a2fc70f20639c67c67
Narciso Gomes <narcisoalmeida25@gmail.com>
2024-06-07 23:29:41 -0300

    ADD: config jest env development

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 const nextJest = require("next/jest");
-
+const dotenv = require("dotenv");
+dotenv.config({
+  path: ".env.development",
+});
 const createJestConfig = nextJest();
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
--- tests/integrations/api/v1/migrations/post.test.js
+++ tests/integrations/api/v1/migrations/post.test.js
@@ -1,18 +1,16 @@
-test("POST to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status", {
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
   expect(response.status).toBe(200);
-
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
-
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-
-  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+  expect(Array.isArray(responseBody)).toBe(true);
 });
